f29878f45c2a21a0f5af4d821ee9427b529b6419
Robert Petryszak <rpetry@ebi.ac.uk>
2013-10-15 15:34:03 +0100

    Re-instated rnseq check for file headers

--- prepare_arraydesigns_forloading.sh
+++ prepare_arraydesigns_forloading.sh
@@ -13,9 +13,8 @@ IFS="
 
 function removeDuplicateMappigs {
     f=$1
-    mv $f ${f}.original
+    cp $f ${f}.original
 
-    echo "Processing $f..."
     tail -n +2 ${f} | sort -k 1 | sort > ${f}.aux
     cat ${f}.aux | awk '{print $2}' | sort | uniq -c | sort -k 1 --numeric | grep -v '^      1' | awk '{print $2}' | xargs -I % grep '%$' ${f}.aux | sort -k 1 > ${f}.duplicatemappings.aux
     head -1 ${f}.original > $f
@@ -28,31 +27,31 @@ function removeDuplicateMappigs {
 	exit 1
     else
         rm -rf ${f}*.aux
-        # rm -rf ${f}  	
+        # rm -rf ${f}.original  	
     fi
 }
 
 pushd $outputDir/ensembl
-type="gene"
 for f in $(ls *A-*.tsv); do 
+    echo "Removing desingn elements mapped to multiple genes from $f..."
     removeDuplicateMappigs $f
+done
+
+for f in $(ls *A-*.tsv); do 
     arrayDesign=`echo $f | awk -F"." '{print $2}'`
     for l in $(tail -n +2 $f); do
-	identifier=`echo $l | awk -F"\t" '{print $1}'`
-	designElement=`echo $l | awk -F"\t" '{print $2}'`
-	echo -e "${designElement}\t${identifier}\t${type}\t${arrayDesign}" >> $out
+	designElementIdentifierType=`echo $l | awk -F"\t" '{print $2"\t"$1"\tgene"}'`
+	echo -e "${designElementIdentifierType}\t${arrayDesign}" >> $out
     done
 done
 popd
 
 pushd $outputDir/mirbase
-type="mature_miRNA"
 for f in $(ls *A-*.tsv); do 
     arrayDesign=`echo $f | awk -F"." '{print $2}'`
     for l in $(tail -n +2 $f); do
-        identifier=`echo $l | awk -F"\t" '{print $1}'`
-        designElement=`echo $l | awk -F"\t" '{print $2}'`
-        echo -e "${designElement}\t${identifier}\t${type}\t${arrayDesign}" >> $out
+        designElementIdentifierType=`echo $l | awk -F"\t" '{print $2"\t"$1"\tmature_miRNA"}'`
+	echo -e "${designElementIdentifierType}\t${arrayDesign}" >> $out
     done
 done 
 popd
