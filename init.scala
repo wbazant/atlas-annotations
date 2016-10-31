0902e0c2558a09bc654e2d4a6af5ac849cc4da41
Alfonso Muñoz-Pomer <amunoz@ebi.ac.uk>
2016-10-31 12:39:35 +0000

    Add @ to split script into two compilation units; use SpeciesFactory.create

--- init.scala
+++ init.scala
@@ -1,11 +1,13 @@
 //you can quickly install Oracle drivers through these magic imports
-//import $ivy.`com.oracle:ojdbc6:11.2.0.3`
-//import $ivy.`com.oracle:ucp:11.2.0.3`
+import $ivy.`com.oracle:ojdbc6:11.2.0.3`
+import $ivy.`com.oracle:ucp:11.2.0.3`
 
 import ammonite.ops._
 import ammonite.ops.ImplicitWd._
 interp.load.cp(ammonite.ops.pwd/"lib")
 ls! pwd/'lib |! interp.load.cp //load all the jars
+
+@
 
 // set up the database
 val ds = oracle.ucp.jdbc.PoolDataSourceFactory.getPoolDataSource()
@@ -19,10 +21,11 @@ ds.setMaxIdleTime(1)
 val jdbcTemplate = new org.springframework.jdbc.core.JdbcTemplate(ds)
 
 // use objects modelling our problem domain
-val human = new uk.ac.ebi.atlas.trader.SpeciesFactory(jdbcTemplate).getSpecies("homo sapiens")
+val sf = new uk.ac.ebi.atlas.trader.SpeciesFactory(jdbcTemplate) 
+val human = sf.create("homo sapiens")
 
 //solr setup is much less fiddly - the client/query part is just a wrapper around a URL
-val analyticsSolrClient = new org.apache.solr.client.solrj.impl.HttpSolrClient("http://localhost:8983/solr/analytics")
+val analyticsSolrClient = new org.apache.solr.client.solrj.impl.HttpSolrClient("http://lime:8983/solr/analytics")
 
 val analyticsSearchService = new uk.ac.ebi.atlas.search.analyticsindex.AnalyticsSearchService(new uk.ac.ebi.atlas.search.analyticsindex.AnalyticsIndexSearchDAO(new uk.ac.ebi.atlas.search.analyticsindex.solr.AnalyticsClient(analyticsSolrClient)))
 
