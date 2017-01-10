182d937a29190ed370762211631b2270b8b25807
wbazant <wbazant@ebi.ac.uk>
2017-01-10 11:58:23 +0000

    Log to stdout

--- sh/fetch-annotations.sh
+++ sh/fetch-annotations.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # This script calls the Ammonite code that fetches annotations.
 
-LOG=$ATLAS_PROD/log
-mkdir -p LOG
-
 pushd `dirname $0`/..
 
-amm -s -c "import \$file.^.src.pipeline.main; main.runAll(\"$LOG\")" > $LOG/amm.log
+amm -s -c 'import $file.src.pipeline.main; main.runAll()'
 
 popd
