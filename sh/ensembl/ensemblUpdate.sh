1a9ed63548c69372f4ae8a25a2a6a4c71ee0680a
wbazant <wbazant@ebi.ac.uk>
2017-01-18 11:13:39 +0000

    Sort the files into Unix sort (hack)

--- sh/ensembl/ensemblUpdate.sh
+++ sh/ensembl/ensemblUpdate.sh
@@ -44,6 +44,13 @@ if [ $? -ne 0 ]; then
 fi
 popd
 
+echo "Sorting the outputs"
+# sadly the "join" in decorations package expects the Unix sort, the Scala one is different and it breaks
+function sortInPlace(){
+  sort -k1,1 $1 -o $1
+}
+cat <(find -L ${ATLAS_PROD}/bioentity_properties/ensembl -name '*tsv') <(find -L ${ATLAS_PROD}/bioentity_properties/wbps -name '*tsv') | xargs -n1 sortInPlace
+
 echo "Merge all individual Ensembl property files into matrices"
 for species in $(ls $PROJECT_ROOT/annsrcs/ensembl | awk -F"." '{print $1}' | sort | uniq); do
     for bioentity in ensgene enstranscript ensprotein; do
