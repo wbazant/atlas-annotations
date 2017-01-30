72d947f2337b31e2d12bce6f7897cc4d2c292b5d
wbazant <wbazant@ebi.ac.uk>
2017-01-30 13:37:45 +0000

    Use grep -E as it is specified by POSIX ; change directories after moving the files so that we can refer to files relative to script file

--- sh/reactome/fetchAllReactomeMappings.sh
+++ sh/reactome/fetchAllReactomeMappings.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script retrieves Ensembl gene identifier - Reactome pathway identifier - Reactome pathway name triples and puts them in ${organism}.reactome.tsv files, depending on the organism of the Ensembl gene identifier
 # Author: rpetry@ebi.ac.uk
+# Note the script uses GNU's extended awk
 
 outputDir=$1
 if [[ -z "$outputDir" ]]; then
@@ -18,7 +19,6 @@ function find_properties_file() {
     | head -n1
 }
 
-pushd $outputDir
 IFS="
 "
 
@@ -30,14 +30,16 @@ IFS="
 start=`date +%s`
 # Please note that the two files are currently provided manually by Justin Preece from Gramene project
 # we check them in and keep them with source code
-cat `dirname $0`/UniProt2PlantReactome_All_Levels.txt | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | sort -k 1,1 > aux.UniProt2PlantReactome
-cat `dirname $0`/Ensembl2PlantReactome_All_Levels.txt | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | sort -k 1,1 > aux.Ensembl2PlantReactome
+cat `dirname $0`/UniProt2PlantReactome_All_Levels.txt | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | sort -k 1,1 > $outputDir/aux.UniProt2PlantReactome
+cat `dirname $0`/Ensembl2PlantReactome_All_Levels.txt | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | sort -k 1,1 > $outputDir/aux.Ensembl2PlantReactome
 #Download the other files
-curl -s -X GET "http://www.reactome.org/download/current/UniProt2Reactome_All_Levels.txt" | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | sort -k 1,1 > aux.UniProt2Reactome
+curl -s -X GET "http://www.reactome.org/download/current/UniProt2Reactome_All_Levels.txt" | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | sort -k 1,1 > $outputDir/aux.UniProt2Reactome
 # Ensembl2Reactome appears to map to pathways a combination of gene and transcript identifiers (I've seen evidence of a gene and its transcript
 # being mapped to the same pathway in separate lines of the same file). Exluding transcript identifers to avoid Solr index (that consumes these files)
 # from being corrupted.
-curl -s -X GET "http://www.reactome.org/download/current/Ensembl2Reactome_All_Levels.txt" | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | grep -v '^ENST' | sort -k 1,1 > aux.Ensembl2Reactome
+curl -s -X GET "http://www.reactome.org/download/current/Ensembl2Reactome_All_Levels.txt" | awk -F"\t" '{print $1"\t"$6"\t"$2"\t"$4}' | grep -Ev '^ENST' | sort -k 1,1 > $outputDir/aux.Ensembl2Reactome
+
+pushd $outputDir
 
 for file in Ensembl2Reactome UniProt2Reactome UniProt2PlantReactome Ensembl2PlantReactome; do
   # Lower-case and replace space with underscore in all organism names; create files with headers for each organism
@@ -66,7 +68,7 @@ for file in UniProt2Reactome UniProt2PlantReactome; do
     uniprotMappingFile=$(find_properties_file $lcOrganism "uniprot")
     if [ -s $uniprotMappingFile ]; then
       # Now prepare Ensembl's UniProt to Ensembl mapping file - in the right order, ready for joining with the $lcOrganism portion of Ensembl2Reactome
-      grep -v '\t$' $uniprotMappingFile | awk -F"\t" '{print $2"\t"$1}' | sort -k1,1 | uniq > aux.${lcOrganism}.ensembl.tsv.tmp
+      grep -E '^\w+\W\w+$' $uniprotMappingFile | awk -F"\t" '{print $2"\t"$1}' | sort -u -k1,1 > aux.${lcOrganism}.ensembl.tsv.tmp
       # Join to Ensmebl mapping file, then remove protein accessions before appending the UniProt only-annotated pathways to ${lcOrganism}.reactome.tsv
       join -t $'\t' -1 1 -2 1 aux.${lcOrganism}.ensembl.tsv.tmp aux.${lcOrganism}.$file.tsv.tmp | awk -F"\t" '{print $2"\t"$3"\t"$4}' >> ${lcOrganism}.reactome.tsv.tmp
     fi
