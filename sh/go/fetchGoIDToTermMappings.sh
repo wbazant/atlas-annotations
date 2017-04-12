448eb18418811f1e2228a4bf112b1e5ca510fec3
wbazant <wbazant@ebi.ac.uk>
2017-04-12 11:29:59 +0100

    Make fetching properties from OWL slightly generic and use it for extracting ontology terms too. Integrate into GO master script and set the script to strict mode

--- sh/go/fetchGoIDToTermMappings.sh
+++ sh/go/fetchGoIDToTermMappings.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 # This script retrieves the latest mapping between GO ids and terms
-# Author: rpetry@ebi.ac.uk
-
+# Author: rpetry@ebi.ac.uk, wbazant@ebi.ac.uk
+set -euo pipefail
+PROJECT_ROOT=`dirname $0`/../..
+export JAVA_OPTS=-Xmx3000M
 IFS="
 "
 outputDir=$1
@@ -9,101 +12,30 @@ if [[ -z "$outputDir" ]]; then
     exit 1
 fi
 
-get_ontology_id2term_mappings() {
-    ontology=$1
-    owlFileUrl=$2
-    outputDir=$3
+echo "Fetching GO and PO owl files"
+#curl -s "http://geneontology.org/ontology/go.owl" > $outputDir/go.owl
+#curl -s "http://palea.cgrb.oregonstate.edu/viewsvn/Poc/tags/live/plant_ontology.owl?view=co" > $outputDir/po.owl
 
-    pushd $outputDir
-    rm -rf ${ontology}.owl*
-    curl -s -o ${ontology}.owl -X GET "$owlFileUrl"
-    if [ $? -ne 0 ]; then
-	echo "ERROR: Failed to retrieve: ${ontology}.owl" >&2 
-	return 1
-    fi
+pushd $PROJECT_ROOT > /dev/null
+echo "Extracting GO id -> term"
+amm -s -c "import \$file.src.go.PropertiesFromOwlFile; PropertiesFromOwlFile.terms(\"$outputDir/go.owl\")" \
+  > $outputDir/goIDToTerm.tsv
 
-    # Retrieve id's and terms separately, then (with the assumption they come back in the same order as each other)
-    # join by line number; then loose the line number
-    xml_grep owl:Class/rdfs:label ${ontology}.owl --text_only | grep '' -n > ${ontology}.term.aux
-    xml_grep owl:Class/oboInOwl:id ${ontology}.owl --text_only | grep '' -n > ${ontology}.id.aux
-    perl -pi -e 's|(^\d+):|$1\t|g' ${ontology}.term.aux
-    perl -pi -e 's|(^\d+):|$1\t|g' ${ontology}.id.aux
-    join -t $'\t' -1 1 -2 1 ${ontology}.id.aux ${ontology}.term.aux | awk -F"\t" '{print $2"\t"$3}' > ${ontology}IDToTerm.tsv
+echo "Extracting PO id -> term"
+amm -s -c "import \$file.src.go.PropertiesFromOwlFile; PropertiesFromOwlFile.terms(\"$outputDir/po.owl\")" \
+  > $outputDir/poIDToTerm.tsv
 
-    rm -rf ${ontology}.*.aux
-    popd
-}
-
-get_ontology_alternativeID2canonicalID_mappings() {
-    ontology=$1
-    owlFileUrl=$2
-    outputDir=$3
-
-    pushd $outputDir
-    rm -rf ${ontology}.owl*
-    curl -s -o ${ontology}.owl -X GET "$owlFileUrl"
-    if [ $? -ne 0 ]; then
-	echo "ERROR: Failed to retrieve: ${ontology}.owl" >&2 
-	return 1
-    fi
-
-    grep -P 'owl:Class|oboInOwl:id |oboInOwl:hasAlternativeId ' go.owl | perl -p -e 's|^.*oboInOwl:(.*)\s.*\>(.*)\<.*$|$1\t$2|' > ${ontology}.canonicalAlternativeID.aux
-    # Example content of ${ontology}.canonicalAlternativeID.aux:
-    # id\tGO:0035195
-    # hasAlternativeId\tGO:0030918
-    for l in $(cat ${ontology}.canonicalAlternativeID.aux); do
-	echo $l | grep 'owl:Class ' > /dev/null
-	if [ $? -eq 0 ]; then
-	    alternativeIds=
-	    id=
-	fi
-
-	echo $l | grep '^id' > /dev/null
-	if [ $? -eq 0 ]; then
-	    id=`echo $l | awk -F"\t" '{print $2}'`
-	fi
-	echo $l | grep '^hasAlternativeId' > /dev/null
-	if [ $? -eq 0 ]; then
-	    alternativeIds="$alternativeIds "`echo $l | awk -F"\t" '{print $2}'`
-	fi
-	echo $l | grep '/owl:Class' > /dev/null
-	if [ $? -eq 0 ]; then
-	    if [ ! -z "$id" ]; then
-		for alternativeId in $(echo $alternativeIds | tr " " "\n" | sed '/^$/d'); do
-		    echo -e "${alternativeId}\t${id}"
-	        done
-	    elif [ ! -z $alternativeIds ]; then 
-		echo "ERROR: Encountered an alternative id: $alternativeId when canonical id has not been found"
-		return 1
-	    fi
-	fi
-    done > ${ontology}.alternativeID2CanonicalID.tsv
-    rm -rf ${ontology}.canonicalAlternativeID.aux
-    popd
-}
+echo "Extracting GO alternativeId -> id"
+amm -s -c "import \$file.src.go.PropertiesFromOwlFile; PropertiesFromOwlFile.alternativeIds(\"$outputDir/go.owl\")" \
+  > $outputDir/go.alternativeID2CanonicalID.tsv
+popd > /dev/null
 
 # This is just for GO (i.e. not PO)
 get_ontology_id2Depth_mappings() {
     echo "SELECT term.acc, term.term_type, min(graph_path.distance) FROM term INNER JOIN graph_path ON (term.id=graph_path.term2_id) INNER JOIN term AS ancestor ON (ancestor.id=graph_path.term1_id) AND ancestor.is_root=1 group by term.acc, term.term_type" | mysql --silent -hmysql-amigo.ebi.ac.uk -ugo_select -pamigo -P4085 go_latest | grep '^GO:'  | sort -t$'\t' -k1,1
 }
 
-get_ontology_id2term_mappings go "http://geneontology.org/ontology/go.owl" $outputDir
-if [ $? -ne 0 ]; then
-   exit 1
-fi
-get_ontology_alternativeID2canonicalID_mappings go "http://geneontology.org/ontology/go.owl" $outputDir
-if [ $? -ne 0 ]; then
-   exit 1
-fi
-get_ontology_id2term_mappings po "http://palea.cgrb.oregonstate.edu/viewsvn/Poc/tags/live/plant_ontology.owl?view=co" $outputDir
-if [ $? -ne 0 ]; then
-   exit 1
-fi
 get_ontology_id2Depth_mappings > $outputDir/goIDToDepth.tsv
-if [ $? -ne 0 ]; then
-   exit 1
-fi
-
 
 
 # Append Plant Ontology terms at the end of the Gene Ontology file (Ensembl provides Plant Ontology (PO) and Gene Ontology (GO) terms - as GO terms)
---
+++ src/go/PropertiesFromOwlFile.sc
@@ -0,0 +1,78 @@
+/*
+Input: location of go.owl or po.owl downloaded from http://geneontology.org/ontology/go.owl
+<owl:Class>
+    <rdfs:label>embryo proper</rdfs:label>
+    <oboInOwl:hasAlternativeId>GO:0019952</oboInOwl:hasAlternativeId>
+    <oboInOwl:hasAlternativeId>GO:0050876</oboInOwl:hasAlternativeId>
+    <oboInOwl:id>GO:0000003</oboInOwl:id>
+</owl:Class>
+
+//alternativeIds output
+GO:0019952\tGO:0000003
+GO:0050876\tGO:0000003
+
+//terms output
+GO:0000003\tembryo proper
+*/
+import scala.io.Source
+import scala.xml.pull._
+
+type Result = (String, String)
+case class OwlClassResult(maybeId: Option[String] = None, properties: List[String] = List()) {
+  def noteId(id: String) = OwlClassResult(Some(id), properties)
+  def noteProperty(property: String) = OwlClassResult(maybeId, property :: properties)
+  def toList : List[Result] = maybeId.map{ case id => properties.map{case property => (id,property)}}.toList.flatten
+}
+
+sealed trait ReadingNodeText
+case object ReadingId extends ReadingNodeText
+case object ReadingProperty extends ReadingNodeText
+case object NotReadingNodeText extends ReadingNodeText
+
+
+type X = (Option[OwlClassResult],ReadingNodeText, List[Result])
+val xInit : X = (None, NotReadingNodeText, List[(String,String)]())
+def parseStep(propertyNamespace: String, property:String)(x: X, evt: XMLEvent) : X = {
+  val (currentOwlClass, isReadingNodeText, acc) = x
+  (evt,currentOwlClass, isReadingNodeText) match {
+    case (EvElemStart("owl", "Class", _ , _),None, _)
+      => (Some(OwlClassResult()), NotReadingNodeText, acc )
+    case (EvElemStart(pns, p, _, _),Some(owl), _) if pns == propertyNamespace && p == property
+      => (currentOwlClass,ReadingProperty, acc)
+    case (EvElemStart("oboInOwl", "id", _, _),Some(owl), _)
+      => (currentOwlClass,ReadingId, acc)
+    case (EvText(t), Some(owl), ReadingProperty)
+      => (Some(owl.noteProperty(t)),NotReadingNodeText, acc)
+    case (EvText(t), Some(owl), ReadingId)
+      => (Some(owl.noteId(t)), NotReadingNodeText, acc)
+    case (EvElemEnd("owl", "Class"),Some(owl), _)
+      => (None, NotReadingNodeText, owl.toList ::: acc )
+    case _
+      => x
+  }
+}
+
+
+def parse(propertyNamespace: String, property:String)(fileLocation: String) = {
+  new XMLEventReader(Source.fromFile(fileLocation))
+  .foldLeft(xInit)(parseStep(propertyNamespace, property))
+  ._3
+  .reverse
+}
+
+def terms(fileLocation: String) = {
+  parse("rdfs", "label")(fileLocation)
+  .foreach{
+    case (id, goTermText)
+      => println(s"${id}\t${goTermText}")
+  }
+}
+
+def alternativeIds(fileLocation: String) = {
+  parse("oboInOwl", "hasAlternativeId")(fileLocation)
+  .map(_.swap)
+  .foreach{
+    case (alternativeId, id)
+      => println(s"${alternativeId}\t${id}")
+  }
+}
