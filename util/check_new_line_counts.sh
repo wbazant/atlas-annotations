fedc5dc668f73ad5e227be33527e6ce1f95270bf
wbazant <wbazant@ebi.ac.uk>
2017-09-25 16:53:26 +0100

    Sketch implementation of the future check_new_line_counts.sh

--- util/check_new_line_counts.sh
+++ util/check_new_line_counts.sh
@@ -1,3 +1,25 @@
+
+
+if [ $# -lt 2 ] ; then
+	echo >&2 "Usage: $0 <previous archive> <current archive>"
+	exit 1
+fi
+
+previousArchive=$1
+currentArchive=$2
+
+fileSizes() {
+	for file in $( find $1 -type f) ; do
+		echo -n $(basename $file) ; stat $file --printf=" %s\n";
+ 	done
+}
+
+join -1 1 -2 1 <(fileSizes $1 | sort ) <(fileSizes $2 | sort )
+
+#TODO make some kind of calculation on the two lists, and fail if annotations differ in sizes
+#TODO also print out annotations present in one but not other?
+
+# Old:
 #TODO I used to live in ensemblUpdate.sh and I am a good idea. Make me useful again if you need to!
 #--------------------------------------------------
 # # Compare the line counts of the new files against those in the previous
